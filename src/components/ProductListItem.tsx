8a1891aad4bb05d9fba42ac035a4bf021baa92d5
kkamara <kamaracomputers@gmail.com>
2024-03-30 15:12:48 +0000

    Creating screens with Expo Router

--- src/components/ProductListItem.tsx
+++ src/components/ProductListItem.tsx
@@ -3,9 +3,11 @@ import {
   Text,
   View,
   Image,
+  Pressable,
 } from 'react-native';
 import Colors from '@/constants/Colors';
 import { Product } from '@/types';
+import { Link, } from 'expo-router';
 
 export const defaultPizzaImage = 
   'https://notjustdev-dummy.s3.us-east-2.amazonaws.com/food/default.png'
@@ -16,16 +18,18 @@ type ProductListItemProps = {
 
 const ProductListItem = ({ product, }: ProductListItemProps) => {
   return (
-    <View style={styles.container}>
-      <Image
-        source={{ uri: product.image || defaultPizzaImage }}
-        style={styles.image}
-        resizeMode='contain'
-      />
+    <Link href={`/${product.id}`} asChild>
+      <Pressable style={styles.container}>
+        <Image
+          source={{ uri: product.image || defaultPizzaImage }}
+          style={styles.image}
+          resizeMode='contain'
+        />
 
-      <Text style={styles.title}>{product.name}</Text>
-      <Text style={styles.price}>${product.price}</Text>
-    </View>
+        <Text style={styles.title}>{product.name}</Text>
+        <Text style={styles.price}>${product.price}</Text>
+      </Pressable>
+    </Link>
   );
 };
 
