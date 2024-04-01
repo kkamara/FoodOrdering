2d396ba2bb80687fc78181807090339b95569e76
kkamara <kamaracomputers@gmail.com>
2024-04-01 19:43:05 +0100

    Working with Text Inputs and Forms

--- src/app/(admin)/menu/create.tsx
+++ src/app/(admin)/menu/create.tsx
@@ -2,13 +2,75 @@ import {
   View,
   Text,
   StyleSheet,
+  TextInput,
 } from 'react-native'
-import React from 'react'
+import React, { useState } from 'react'
+import Button from '@/components/Button';
 
 const CreateProductScreen = () => {
+  const [name, setName] = useState("");
+  const [price, setPrice] = useState("");
+
+  const [errors, setErrors] = useState("");
+
+  const resetFields = () => {
+    setName("");
+    setPrice("");
+  };
+
+  const validateInput = () => {
+    setErrors("");
+    if (!name) {
+      setErrors("Name is required");
+      return false;
+    }
+    if (!price) {
+      setErrors("Price is required");
+      return false;
+    }
+    if (isNaN(parseFloat(price))) {
+      setErrors("Price is not a number");
+      return false;
+    }
+
+    return true;
+  };
+
+  const onCreate = () => {
+    if (!validateInput()) {
+      return;
+    }
+    console.warn("Creating product: ",name);
+
+    // Save in the database
+
+    resetFields();
+  };
+
   return (
     <View style={styles.container}>
-      <Text>create</Text>
+      <Text style={styles.label}>Name</Text>
+      <TextInput
+        value={name}
+        onChangeText={setName}
+        placeholder="Name"
+        style={styles.input}
+      />
+      
+      <Text style={styles.label}>Price ($)</Text>
+      <TextInput
+        value={price}
+        onChangeText={setPrice}
+        placeholder="9.99"
+        style={styles.input}
+        keyboardType='numeric'
+      />
+
+      <Text style={{ color: "red", }}>
+        {errors}
+      </Text>
+
+      <Button onPress={onCreate} text="Create" />
     </View>
   )
 };
@@ -18,6 +80,17 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: "center",
     padding: 10,
+  },
+  input: {
+    backgroundColor: "white",
+    padding: 10,
+    borderRadius: 5,
+    marginTop: 5,
+    marginBottom: 20,
+  },
+  label: {
+    color: "gray",
+    fontSize: 16,
   },
 });
 
