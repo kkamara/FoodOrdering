8da216958545aa867be01a453b3cde8a5fe2488e
kkamara <kamaracomputers@gmail.com>
2024-04-01 20:01:54 +0100

    Product screen overview

--- src/app/(admin)/menu/create.tsx
+++ src/app/(admin)/menu/create.tsx
@@ -3,15 +3,20 @@ import {
   Text,
   StyleSheet,
   TextInput,
+  Image,
 } from 'react-native'
 import React, { useState } from 'react'
 import Button from '@/components/Button';
+import { defaultPizzaImage } from '@/components/ProductListItem';
+import Colors from '@/constants/Colors';
+import * as ImagePicker from 'expo-image-picker';
+import { Stack } from 'expo-router';
 
 const CreateProductScreen = () => {
   const [name, setName] = useState("");
   const [price, setPrice] = useState("");
-
   const [errors, setErrors] = useState("");
+  const [image, setImage] = useState<string|null>(null);
 
   const resetFields = () => {
     setName("");
@@ -47,8 +52,31 @@ const CreateProductScreen = () => {
     resetFields();
   };
 
+  const pickImage = async () => {
+    // No permissions request is necessary for launching the image library
+    let result = await ImagePicker.launchImageLibraryAsync({
+      mediaTypes: ImagePicker.MediaTypeOptions.Images,
+      allowsEditing: true,
+      aspect: [4, 3],
+      quality: 1,
+    });
+
+    if (!result.canceled) {
+      setImage(result.assets[0].uri);
+    }
+  };
+
   return (
     <View style={styles.container}>
+      <Stack.Screen
+        options={{ title: "Create Product", }}
+      />
+      <Image
+        source={{ uri: image || defaultPizzaImage, }}
+        style={styles.image}
+      />
+      <Text onPress={pickImage} style={styles.textButton}>Select Image</Text>
+
       <Text style={styles.label}>Name</Text>
       <TextInput
         value={name}
@@ -80,6 +108,17 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: "center",
     padding: 10,
+  },
+  image: {
+    width: "50%",
+    aspectRatio: 1,
+    alignSelf: "center",
+  },
+  textButton: {
+    alignSelf: "center",
+    fontWeight: "bold",
+    color: Colors.light.tint,
+    marginVertical: 10,
   },
   input: {
     backgroundColor: "white",
