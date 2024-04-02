1c48db974bd2d9608eeb1f5eb992e3d2192cb264
kkamara <kamaracomputers@gmail.com>
2024-04-02 16:15:02 +0100

    Add orders top tabs navigator

--- src/app/(admin)/orders/_layout.tsx
+++ src/app/(admin)/orders/_layout.tsx
@@ -3,9 +3,9 @@ import { Stack, } from "expo-router";
 export default function MenuStack() {
     return <Stack>
         <Stack.Screen
-            name="index"
+            name="list"
             options={{
-                title: "Orders",
+                headerShown: false,
             }}
         />
     </Stack>;
