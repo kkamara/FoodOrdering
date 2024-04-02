69d826c21be6b9fee1df2abd7f656ea04e30bf3f
kkamara <kamaracomputers@gmail.com>
2024-04-02 15:08:46 +0100

    Add orders screens

--- src/app/(user)/menu/_layout.tsx
+++ src/app/(user)/menu/_layout.tsx
@@ -1,6 +1,6 @@
-import { FontAwesome } from "@expo/vector-icons";
-import { Link, Stack } from "expo-router";
-import { Pressable } from "react-native";
+import { FontAwesome, } from "@expo/vector-icons";
+import { Link, Stack, } from "expo-router";
+import { Pressable, } from "react-native";
 import Colors from "@/constants/Colors";
 
 export default function MenuStack() {
